f29594289977b39fd23b5b9fc7b576a1b2ee857d
Alassane NIANG <adniang75@gmail.com>
2022-02-15 23:14:02 +0100

    Initial Commit

---
+++ src/test/kotlin/com/alassaneniang/kotlinsearch/KotlinSearchApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.alassaneniang.kotlinsearch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSearchApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
